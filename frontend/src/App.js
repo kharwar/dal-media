46ee3ffa1f380dcd766dc5c31eebcd0e3ef252ee
Ridham Kathiriya <rd323302@dal.ca>
2022-07-06 16:28:58 -0300

    front end api services added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,13 @@ import { BrowserRouter } from "react-router-dom";
 import AlertDialog, { AlertProvider } from "./components/alert-dialog";
 import { snackbarRef } from "./components/material-snackbar";
 import { AuthContext } from "./context";
+import { ServiceManager, apiRoutes } from "./services";
 function App() {
   const [isLogin, setLogin] = useState(false);
+
+  useEffect(() => {
+    ServiceManager.initialize(apiRoutes.baseURL);
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
---
+++ frontend/src/services/api-routes.js
@@ -0,0 +1,8 @@
+export default {
+  baseURL: "http://localhost:8000/api/",
+  getPosts: "posts",
+  createPost: "posts/create",
+
+  getEvents: "events",
+  createEvent: "events/create",
+};
