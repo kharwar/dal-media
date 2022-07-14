fb3a25f9b071414e43024f52f442cb7ce1bb4826
Tasnim Khan <ts802838@dal.ca>
2022-07-14 18:53:06 -0300

    Group Backend. WIP

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,9 +1,11 @@
 const userController = require("./user/controller.user");
 const postController = require("./post/controller.post");
 const blogController = require("./blog/controller.blog");
+const groupController = require("./group/controller.group");
 
 module.exports = {
   userController,
   blogController,
   postController,
+  groupController,
 };
